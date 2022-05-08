50d62b5d066d67ef48e87bcc292ce25ab4dfdaf9
Davi Miguel <52977678+davi-miguel-sm@users.noreply.github.com>
2022-05-08 20:25:22 -0300

    Finalizei o rodapé com responsividade e adicionei icones à sidebar

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,13 +1,13 @@
-import Content from '../components/template/Content';
-import Footer from '../components/template/Footer';
-import Header from '../components/template/Header';
-import SideMenu from '../components/template/SideMenu';
+import Content from '../components/template/content/Content';
+import Footer from '../components/template/footer/Footer';
+import Header from '../components/template/header/Header';
+import SideMenu from '../components/template/sidemenu/SideMenu';
 
 import '../styles/global.css';
 
 export default function App() {
 	return (
-		<div className='app '>
+		<div className='app'>
 			<Header />
 			<SideMenu />
 			<Content />
